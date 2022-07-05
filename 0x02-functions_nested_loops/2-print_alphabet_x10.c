cfc97692dd401c9522c1e1aab8743a1e276686fe
Austin <austin5astro@gmail.com>
2022-07-05 14:27:08 +0000

    Fixed code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * print_alphabet_x10 - Prints all the letters of the alphabets in 
+  * print_alphabet_x10 - Prints all the letters of the alphabets in
   * lowercase 10 times
   *
   * Return: 0 for success
