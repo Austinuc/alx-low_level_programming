70c0162eff820fab38f88b739f6a9f0cd0ce26b1
Austin <austin5astro@gmail.com>
2022-07-05 23:08:08 +0000

    Code cleanup

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,23 +8,15 @@
 void print_times_table(int n)
 {
 	int i = 0;
-	int j;
+	int j, k;
 
-	if ((n > 15) || (n < 0)) /* don't run for n < 0 or n >15 */
+	if ((n >= 0) && (n <= 15)) /* don't run for n < 0 or n >15 */
 	{
-		return;
-	}
-	else
-		while (i <= n) /* loop through n to get range as columns eg: ix , ix, ... */
+		for (i = 0; i <= n; i++) /* loop: ix , ix, ... */
 		{
-			for (j = 0; j <= n; j++) /* get the row multiples for each colum eg: ix1, ix2, ..., ixn */
+			for (j = 0; j <= n; j++) /* get the row: ix1, ix2, ..., ixn */
 			{
-				int k = i * j; /* multiplication table vals */
-
-				/*
-				   * k grows in value from single digits to multiple digits
-				   * for n <= 15 && n > 0; max digits for each k is 3, ie 15x15=225
-				   */
+				k = i * j; /* multiplication table vals */
 				if (k < 10) /*print single digits from 0-9 */
 				{
 					if (j != 0) /*rm space from 1st col*/
@@ -33,50 +25,29 @@ void print_times_table(int n)
 						_putchar(' ');
 					}
 					_putchar('0' + k);
-
-					if (j != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
 				}
-
-				if ((k >= 10) && (k < 100)) /* Print double digits from 10-99 */
+				if ((k >= 10) && (k < 100)) /* Prints from 10-99 */
 				{
 					if (j != 0) /*rm space from 1st col*/
 					{
 						_putchar(' ');
 					}
-
-					/*print last 2 digit*/
 					_putchar('0' + (k / 10));
 					_putchar('0' + (k % 10));
-
-					if (j != n) /*remove frm last colums*/
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
 				}
-
-				if ((k / 100) >= 1) /* Print tripple digits from 100-999 */
+				if ((k / 100) >= 1) /* Prints from 100-999 */
 				{
 					_putchar('0' + (k / 100)); /*print 1st digit*/
-					k %= 100; /* take only remaining part*/
-
-					/*print rem last 2 digit*/
-					_putchar('0' + (k / 10));
+					_putchar('0' + ((k / 10) % 10));
 					_putchar('0' + (k % 10));
-					
-					if (j != n) /*remove comma and space from last colums*/
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
 				}
-
+				if (j != n) /*rmv space from last col*/
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			_putchar('\n');
-			i++; /* go to next value and compute the multiplication table */
 		}
+	}
 }
