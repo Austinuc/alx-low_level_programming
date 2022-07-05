b316e073117917f68a2ec8a058acb3cff916935c
Austin <austin5astro@gmail.com>
2022-07-05 16:21:59 +0000

    Fixed code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,13 +5,13 @@
   *
   * Return: Last digit of the given n
   */
-int print_last_digit(long n)
+int print_last_digit(int n)
 {
 	if (n < 0)
 	{
 		n *= -1;
 	}
-	_putchar('0' + (n % 10));
+	_putchar(48 + (n % 10));
 
 	return (n % 10);
 }
