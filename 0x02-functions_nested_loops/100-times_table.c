f15efe2b2882137fb879635a3f2ee5385971223d
Austin <austin5astro@gmail.com>
2022-07-05 23:39:21 +0000

    Code cleanup

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,44 +7,35 @@
   */
 void print_times_table(int n)
 {
-	int i = 0;
-	int j, k;
+	int row, column, product;
 
 	if ((n >= 0) && (n <= 15)) /* don't run for n < 0 or n >15 */
 	{
-		for (i = 0; i <= n; i++) /* loop: ix , ix, ... */
+		for (row = 0; row <= n; row++) /* loop: ix , ix, ... */
 		{
-			for (j = 0; j <= n; j++) /* get the row: ix1, ix2, ..., ixn */
+			_putchar(48);
+			for (column = 1; column <= n; column++) /* get the row: ix1, ix2, ..., ixn */
 			{
-				k = i * j; /* multiplication table vals */
-				if (k < 10) /*print single digits from 0-9 */
+				product = row * column; /* multiplication table vals */
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10) /*print single digits from 0-9 */
 				{
-					if (j != 0) /*rm space from 1st col*/
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar('0' + k);
-				}
-				if ((k >= 10) && (k < 100)) /* Prints from 10-99 */
-				{
-					if (j != 0) /*rm space from 1st col*/
-					{
-						_putchar(' ');
-					}
-					_putchar('0' + (k / 10));
-					_putchar('0' + (k % 10));
-				}
-				if ((k / 100) >= 1) /* Prints from 100-999 */
-				{
-					_putchar('0' + (k / 100)); /*print 1st digit*/
-					_putchar('0' + ((k / 10) % 10));
-					_putchar('0' + (k % 10));
-				}
-				if (j != n) /*rmv space from last col*/
-				{
-					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product < 100) /* Prints from 10-99 */
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100) /* Prints from 100-999 */
+				{
+					_putchar('0' + (product / 100)); /*print 1st digit*/
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
 				}
 			}
 			_putchar('\n');
