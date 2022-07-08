397b68e4f6a9e4a61124cfe2a88fd7b13dfae6b7
Austin <austin5astro@gmail.com>
2022-07-08 19:27:52 +0000

    Adding 5-more_numbers.c file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * more_numbers - Prints 0 - 14 ten times
+  * Return: void
+  */
+
+void more_numbers(void)
+{
+	int n = 0;
+	int i = 0;
+
+	while (i <= 10)
+	{
+		for (; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + 48);
+			}
+			_putchar((n % 10) + 48);
+		}
+		i++;
+		n = 0;
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -17,3 +17,4 @@ File name | Description
 2-mul.c | Multiplies two given integers
 3-print_numbers.c | Prints the numbers from 0 to 9
 4-print_most_numbers.c | Prints numbers from 0 to 9 except 2 and 4
+5-more_numbers.c | Prints the numbers from 0 to 14 ten times
