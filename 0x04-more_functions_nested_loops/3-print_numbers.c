a09e76b5a75ef2170b6e9053e59e6e60acbf259e
Austin <austin5astro@gmail.com>
2022-07-08 18:00:34 +0000

    Adding 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - Prints the numbers from 0 to 9
+  * Return: Always 0
+  */
+
+void print_numbers(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		_putchar('0' + c);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -15,3 +15,4 @@ File name | Description
 0-isupper.c | Checks for uppercase character
 1-isdigit.c | Checks for a digit (0 through 9).
 2-mul.c | Multiplies two given integers
+3-print_numbers.c | Prints the numbers from 0 to 9
