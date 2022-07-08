ac9acb1ee9967f13d2f546961bcc57e51511acb4
Austin <austin5astro@gmail.com>
2022-07-08 21:39:07 +0000

    Fixed error

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,19 +9,18 @@
 
 void print_diagonal(int n)
 {
-	int i = 1;
-	int j = 0;
+	int i, j;
 
 	if (!( n <= 0))
 	{
-		for (; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (; j < i; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
 			j = 0;
-			_putchar('\\');
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
