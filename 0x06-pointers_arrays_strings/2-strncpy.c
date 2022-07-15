918581076d30da1b6bdc6c80591202c59ad49af4
Austin <austin5astro@gmail.com>
2022-07-15 02:30:20 +0000

    Checked 2-strncpy file for betty style issues

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int c = 0;
+	int c;
 
-	for (; c < n && src[c] != '\0'; c++)
+	for (c = 0; c < n && src[c] != '\0'; c++)
+	{
 		*(dest + c) = *(src + c);
+	}
 	for (; c < n; c++)
+	{
 		*(dest + c) = '\0';
+	}
 
 	return (dest);
 }
