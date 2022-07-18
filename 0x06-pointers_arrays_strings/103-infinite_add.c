25ffb683347657a6906b3635d77c82b3bcdd6ee5
Austin <austin5astro@gmail.com>
2022-07-18 03:44:16 +0000

    Fixing Betty styling

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,23 +1,20 @@
 #include "main.h"
-
 /**
-  * infinite_add - Adds two numbers given as a string
-  * @n1: 1st given number
-  * @n2: 2nd given number
-  * @r: Buffer for result storage
-  * @size_r: Size of buffer
-  *
-  * Description: added each number digit in the strings n1 and n2
-  * individually from right to left by manipulating the ASCII values to
-  * always give the same result as if it was an int addition.
-  * ASCII addition = n1[i] + (n2[i] - 48) + carry. if addition > 9,
-  * subtract 10 and carry 1 to next digit. if addition <= 9, value is
-  * same and carry is zero.
-  *
-  * Return: Pointer to buffer
-  */
-
-int len(char *);
+ * infinite_add - Adds two numbers given as a string
+ * @n1: 1st given number
+ * @n2: 2nd given number
+ * @r: Buffer for result storage
+ * @size_r: Size of buffer
+ *
+ * Description: added each number digit in the strings n1 and n2
+ * individually from right to left by manipulating the ASCII values to
+ * always give the same result as if it was an int addition.
+ * ASCII addition = n1[i] + (n2[i] - 48) + carry. if addition > 9,
+ * subtract 10 and carry 1 to next digit. if addition <= 9, value is
+ * same and carry is zero.
+ *
+ * Return: Pointer to buffer
+ */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
@@ -35,13 +32,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	len2 = i;
 	i = 1;
-	
+
 	/* get length of max string length */
 	if (len1 >= len2)
 		maxlen = len1;
 	else
 		maxlen = len2;
-	
+
 	/*accounts for '\0' & 1 xtra digit at zero index 4 last carry if any*/
 	if (maxlen + 1 >= size_r)
 	{
@@ -49,9 +46,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 
 	/*set d null terminator for r, uses r[0]-r[maxlen] to store ans note:*/
-	/*the xtra bit for last carry if any at the leftmost digits*/
 	*(r + (maxlen + 1)) = '\0';
-	
+
 	/** start 4rm right of n1[len1 - 1] & n2[len2 - 1] */
 	for (i = 1; i <= maxlen; i++)
 	{
@@ -104,9 +100,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 						carry = 0;
 					}
 				}
+
 				*(r + (maxlen - i)) = carry + 48;
 				break;
-
 			}
 			if (len2 == i) /* for len1 > len2 */
 			{
