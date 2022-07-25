dafa144eaa360bc62b3310994ba0fe640d26baa1
Austin <austin5astro@gmail.com>
2022-07-25 23:40:59 +0000

    fixed code

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,84 +1,67 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
-  * strtow - Splits a string into words
-  * @str: input string
-  *
-  * Return: pointer to an array of strings or NULL if fail
-  */
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ *
+ * Return: no return
+ */
+void ch_free_grid(char **grid, unsigned int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
+	}
+}
 
+/**
+ * strtow - splits a string into words.
+ * @str: string.
+ *
+ * Return: pointer of an array of integers
+ */
 char **strtow(char *str)
 {
-	int i = 0, j, count = 0, k;
-	char **str_split;
+	char **aout;
+	unsigned int c, height, i, j, a1;
 
-	if (!(str) || *str == '\0')
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	while (*(str + i) != '\0') /* get no of words present */
+	for (c = height = 0; str[c] != '\0'; c++)
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			height++;
+	aout = malloc((height + 1) * sizeof(char *));
+	if (aout == NULL || height == 0)
 	{
-		if (*str != ' ') /* if a word is at the begining of the str */
-			count++;
-		if (*(str + i) == ' ' && *(str + i + 1) != ' ' &&
-				*(str + i + 1) != '\0') /* words elsewhere */
-			count++;
-		i++;
-	}
-
-	/* allocate mem space for each word & extra space of last NULL elem */
-
-	str_split = malloc((count + 1) * sizeof(char));
-	if (str_split == NULL)
-	{
-		free(str_split);
+		free(aout);
 		return (NULL);
 	}
-	i = 0;
-
-	/* alloc mem for each character of each word & copy to d alloc space*/
-
-	while (*(str) != '\0')
+	for (i = a1 = 0; i < height; i++)
 	{
-		j = 0, k = 0;
-		if (*str != ' ')
+		for (c = a1; str[c] != '\0'; c++)
 		{
-			while (*str != ' ') /*get the len of each word found*/
+			if (str[c] == ' ')
+				a1++;
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
-				j++;
-				str++;
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
+				if (aout[i] == NULL)
+				{
+					ch_free_grid(aout, i);
+					return (NULL);
+				}
+				break;
 			}
-			str_split[i] = malloc((j + 1) * sizeof(char));
-			if (str_split[i] == NULL)
-			{
-				k = 0;
-				for (; k <= i; k++)
-					free(str_split[k]);
-				free(str_split);
-				return (NULL);
-			}
-
-			/*copy xter from str to the alloc mem space*/
-
-			for (; j >= 0; j--, k++)
-			{
-				if (j == 0) /*terminate each word with NULL*/
-					str_split[i][k] = '\0';
-				else
-					str_split[i][k] = *(str - j);
-			}
-			i++;
 		}
-		str++;
+		for (j = 0; a1 <= c; a1++, j++)
+			aout[i][j] = str[a1];
+		aout[i][j] = '\0';
 	}
-	str_split[count] = malloc(sizeof(char)); /*last elem*/
-	if (str_split[count] == NULL)
-	{
-		i = 0;
-		for (; i <= count; i++)
-			free(str_split[i]);
-		free(str_split);
-		return (NULL);
-	}
-	str_split[count] = '\0';
-	return (str_split);
+	aout[i] = NULL;
+	return (aout);
 }
