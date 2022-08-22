4b8578325205fe355b4f0d434e685f5aa745fd9a
Austin <austin5astro@gmail.com>
2022-08-22 16:04:23 +0000

    [Fix]: fixed header file, code and betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nread, nwrite;
 	char *buf;
 
-	if (!(filename) || (fd = open(filename, O_RDONLY)) == -1)
+	if (!(filename))
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
