711ba7d2415244cf0e2a0f21135c12cda5dab7d3
Austin <austin5astro@gmail.com>
2022-08-23 02:24:24 +0000

    [Feature]: Copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+  * error_check - checks for file errors
+  * @fd_fr: file descriptor of source file
+  * @fd_to: file descriptor of the destination file
+  * @argv: list containing file names
+  *
+  * Return: no return
+  */
+
+void error_check(int fd_fr, int fd_to, char *argv[])
+{
+	if (fd_fr == -1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+  * main - Copies the content of a file to another file
+  * @argc: command line argument count
+  * @argv: array of command line input strings
+  *
+  * Return: 0 on success
+  */
+
+int main(int argc, char *argv[])
+{
+	int fd_fr, fd_to, ncp = 1;
+	char buff[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_fr = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 00664);
+
+	error_check(fd_fr, fd_to, argv);
+
+	while (ncp != 0)
+	{
+		ncp = read(fd_fr, buff, 1024);
+		if (ncp == -1)
+			error_check(-1, 0, argv);
+		ncp = write(fd_to, buff, ncp);
+		if (ncp == -1)
+			error_check(0, -1, argv);
+
+	}
+	if (close(fd_fr) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", fd_fr);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -14,3 +14,5 @@ File name | Description
  --- | ---
 0-read_textfile.c | Reads a text file and prints it to the POSIX standard output
 1-create_file.c | Creates a file if not already existing and writes to the file
+2-append_text_to_file.c | Appends text at the end of a file
+3-cp.c | Copies the content of a file to another file
